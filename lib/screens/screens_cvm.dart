4afb7c07e4daaa42c1863efa0229ebf811103526
MaNoLiN-CRV <manoliin.27@gmail.com>
2024-12-13 10:48:55 +0100

    final

---
+++ lib/screens/screens_cvm.dart
@@ -0,0 +1,4 @@
+export 'package:examen_cvm/screens/homeScreen_cvm.dart';
+export 'package:examen_cvm/screens/alertScreen_cvm.dart';
+export 'package:examen_cvm/screens/loginScreen_cvm.dart';
+export 'package:examen_cvm/screens/ListViewScreen.dart';
